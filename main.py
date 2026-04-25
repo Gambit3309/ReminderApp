5f86b88be0d5aad8d6d76e5fb86654717d887cd2
Gambit3309 <skibrahim3309@gmail.com>
2026-04-25 13:14:15 +0500

    edit reminder function

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from datetime import datetime
 
 menu = ["Add Reminder", "View Reminders","Delete Reminder", "Exit"]
 reminder_list = []
+id = int(0)
 
 def view_reminders():
     if not reminder_list:
         print("Reminders List is empty")
     else:
-        for index, reminder in enumerate(reminder_list, start=1):
-            print(f'{index}. {reminder}')
+        print("%-5s %-20s %-30s %-15s %-20s" % ("ID", "Title", "Description", "Due Date", "Created At"))
+        for reminder in reminder_list:
+            print("%-5s %-20s %-30s %-15s %-20s" %(reminder.get('ID'),reminder.get('title'),reminder.get('Description'),reminder.get('Due Date'),reminder.get('Created at')))
+
+def update_id(id):
+    id = id + 1
+    return id
 
 def add_reminder():
     title = input("Enter Title:")
@@ -17,7 +23,15 @@ def add_reminder():
     due_date = input("Enter Due Date (YYYY-MM-DD): ")
     now = datetime.now()
     creation_date = now.strftime("%Y-%m-%d %H:%M")
-    user_input = title + ' | ' + desc +'\n' + "Due Date: " + due_date + '\nAdded: ' + creation_date
+    #user_input = title + ' | ' + desc +'\n' + "Due Date: " + due_date + '\nAdded: ' + creation_date
+    id_temp = update_id(id)
+    user_input = {
+        'ID' : id_temp,
+        "title" : title,
+        "Description" : desc,
+        "Due Date" : due_date,
+        "Created at" : creation_date
+    }
     reminder_list.append(user_input) 
     print("Reminder Added")
 
@@ -26,9 +40,25 @@ def delete_reminder():
         print("Reminders List is empty")
     else:
         view_reminders()
-        del_reminder = input("Enter reminder number: ")
-        removed = reminder_list.pop(int(del_reminder)-1)  
+        del_reminder = input("Enter reminder ID: ")
+        removed = reminder_list.pop(int(del_reminder))  
         print(f"'{removed}' has been deleted!")
+
+def edit_reminder():
+    if not reminder_list:
+        print("Reminders List is empty")
+    else:
+        view_reminders()
+        select_reminder = int(input("Enter reminder ID: "))
+        keys = list(reminder_list[select_reminder-1].keys())
+        for index, key in enumerate(keys, start=1):
+            print(f"{index}. {key}")
+        user_choice = input("Enter your Field no. to Edit: ")
+        x = input("Enter: ")
+        print(reminder_list[select_reminder-1][int(user_choice)-1])
+        
+            
+
 
 def main():
     while True:
@@ -49,6 +79,8 @@ def main():
             case "4":
                 print("Goodbye!")
                 break
+            case '5':
+                edit_reminder()
             case _:
                 print("Invalid option, try again.") 
 
